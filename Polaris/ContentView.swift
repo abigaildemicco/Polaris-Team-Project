7fc114202db344bf131ddc211d4a5438cebcf9d5
abigaildemicco <abdemicco@gmail.com>
2021-11-15 15:08:17 +0100

    first try

--- Polaris/ContentView.swift
+++ Polaris/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        ZStack{
+            Rectangle()
+                .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                .brightness(0.5)
+                .frame(width: 350, height: 150)
+                .position(x:195, y:250)
+            
+            Rectangle()
+                .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                .brightness(0.5)
+                .frame(width: 350, height: 150)
+                .position(x:195, y:425)
+            Rectangle()
+                .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                .brightness(0.5)
+                .frame(width: 350, height: 150)
+                .position(x:195, y: 600)
+                
+    }//zstack
+        
+}//view
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+} //contentview
