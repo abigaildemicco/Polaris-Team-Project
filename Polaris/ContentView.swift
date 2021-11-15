f26d151c6e566f7f2f9e696b2bcd300dcfcb57b6
abigaildemicco <abdemicco@gmail.com>
2021-11-15 20:23:33 +0100

    rounded corner rettangolo etc

--- Polaris/ContentView.swift
+++ Polaris/ContentView.swift
@@ -8,25 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+
+    var body: some View{
         ZStack{
-            Rectangle()
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
                 .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
                 .brightness(0.5)
                 .frame(width: 350, height: 150)
                 .position(x:195, y:250)
             
-            Rectangle()
+                Text("Mood")
+                    .position(x:50, y:190)
+                Text ("😥      🙁      😐      ☺️      😄")
+                    .position(x: 195, y: 230)
+                
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
                 .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
                 .brightness(0.5)
                 .frame(width: 350, height: 150)
                 .position(x:195, y:425)
-            Rectangle()
+        
+                Text("Reasons")
+                .position(x:60, y: 365)
+            
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
                 .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
                 .brightness(0.5)
                 .frame(width: 350, height: 150)
                 .position(x:195, y: 600)
-                
+
     }//zstack
         
 }//view
