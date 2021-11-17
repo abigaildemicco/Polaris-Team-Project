b650161f9adccf0858ac38c9c4b3def56a10eccf
abigaildemicco <abdemicco@gmail.com>
2021-11-17 09:31:23 +0100

    content view named as feelings calendar

--- Polaris/ContentView.swift
+++ Polaris/ContentView.swift
@@ -15,50 +15,52 @@ struct ContentView: View {
         let startComponents = DateComponents(year: 2020, month: 1, day: 1)
         let endComponents = DateComponents(year: 2021, month: 12, day: 31, hour: 23, minute: 59, second: 59)
         return calendar.date(from:startComponents)!
-            ...
-            calendar.date(from:endComponents)!
+        ...
+        calendar.date(from:endComponents)!
     }()
-
+    
     var body: some View{
         HStack {
             NavigationView {
                 
                 VStack{
                     
-                    ZStack(alignment: .center) {
-                        
-                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
                         .brightness(0.5)
                         .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-                        
-                            DatePicker(
-                                "Start Date",
-                                 selection: $date,
-                                 in: dateRange,
-                                 displayedComponents: [.date]
-                            )
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-                    }
                     
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                    .brightness(0.5)
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-                
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                    .brightness(0.5)
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-        }//vstack
-                .navigationTitle("ciao")
+                    
+                    
+                    
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                        .brightness(0.5)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                    ZStack(alignment: .center) {
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                            .brightness(0.5)
+                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                        DatePicker(
+                            "Start Date",
+                            selection: $date,
+                            in: dateRange,
+                            displayedComponents: [.date]
+                        )
+                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                    } //zstack
+                }//vstack
+                .navigationTitle("Feelings Calendar")
             }
         } //navigationview
-}//view
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    }//view
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
 } //contentview
