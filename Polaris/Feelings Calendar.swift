f981e189c0faec75a9271dd7876a34a6a86b2ba4
abigaildemicco <abdemicco@gmail.com>
2021-11-17 10:44:12 +0100

    Added emoticons

--- Polaris/Feelings Calendar.swift
+++ Polaris/Feelings Calendar.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var date = Date()
+    let emojiSize = 25.0
     let dateRange: ClosedRange<Date> = {
         let calendar = Calendar.current
         let startComponents = DateComponents(year: 2020, month: 1, day: 1)
@@ -20,48 +21,69 @@ struct ContentView: View {
     }()
     
     var body: some View{
-            NavigationView {
-                
-                VStack{ //metto in fila i tre rettangoli
+        NavigationView {
+            
+            VStack{ //metto in fila i tre rettangoli
+                ZStack{
                     RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
                         .brightness(0.5)
                         .frame(width: UIScreen.main.bounds.width - 32, height: 150)
                     
-                    ZStack {
-                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                            .brightness(0.5)
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-                        VStack{
-                            Text ("Family")
-                        }
-                        VStack {
-                            Text ("University")
-                        }
-                        VStack {
-                            Text ("Friends")
-                        }
-                   
-                    } //zstack
-                    
-                    ZStack(alignment: .center) {
-                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                            .brightness(0.5)
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                    HStack (spacing: 10.0){
                         
-                        DatePicker(
-                            "Start Date",
-                            selection: $date,
-                            in: dateRange,
-                            displayedComponents: [.date]
-                        )
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 150)
-                    } //zstack
-                } //vstack
-                .navigationTitle("Feelings Calendar")
-            }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("😢").font(.system(size: emojiSize))
+                        }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("🙁").font(.system(size: emojiSize))
+                        }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("😐").font(.system(size: emojiSize))
+                        }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("☺️").font(.system(size: emojiSize))
+                        }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text("😁").font(.system(size: emojiSize))
+                        }
+                        
+                    } //hstack per le emoji
+                } //zstack primo rettangolo
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                        .brightness(0.5)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                    VStack{
+                        Text ("Family")
+                    }
+                    VStack {
+                        Text ("University")
+                    }
+                    VStack {
+                        Text ("Friends")
+                    }
+                } //zstack secondo rettangolo
+                
+                ZStack(alignment: .center) {
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+                        .brightness(0.5)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                    
+                    DatePicker(
+                        "Start Date",
+                        selection: $date,
+                        in: dateRange,
+                        displayedComponents: [.date]
+                    )
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 150)
+                } //zstack terzo rettangolo
+            } //vstack tutti i rettangoli
+            .navigationTitle("Feelings Calendar")
+        }
     } //view
     
     struct ContentView_Previews: PreviewProvider {
