0ca513a84153f9bff842a971c7607c7426acea6e
AntonioIacono <antonio.iac98@outlook.it>
2021-11-18 17:07:30 +0100

    just some update to ReasonselectionView

--- Polaris/ModelView/EmojiSelectionView.swift
+++ Polaris/ModelView/EmojiSelectionView.swift
@@ -9,27 +9,25 @@ import SwiftUI
 
 struct EmojiSelectionView: View {
     var body: some View {
-        ZStack{
+        
+        
+        VStack{
+            Text("How do you feel?").font(.system(size: 24, weight: .heavy, design: .rounded))
             
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                .brightness(0.65)
-                .frame(width: UIScreen.main.bounds.width - 32, height: 150)
             
-            VStack{
-                Text("How do you feel?").font(.system(size: 24, weight: .heavy, design: .rounded))
-                    
-                
-                
-                HStack{
-                    ForEach(Emojis){ Emoji in
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                            Text(Emoji.name).font(.system(size: Emoji.emojiSize))
-                        }
+            
+            HStack{
+                ForEach(Emojis){ Emoji in
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text(Emoji.name).font(.system(size: Emoji.emojiSize))
                     }
                 }
             }
-        }
+        } .frame(width: UIScreen.main.bounds.width - 32,height: 150)
+            .background(Color.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+            .padding()
+        
     }
 }
 
--- Polaris/ModelView/ReasonSelectionView.swift
+++ Polaris/ModelView/ReasonSelectionView.swift
@@ -10,47 +10,51 @@ import SwiftUI
 struct ReasonSelectionView: View {
     
     private var items: [GridItem] = [
-        GridItem(.fixed(120), spacing: 14),
-        GridItem(.fixed(120), spacing: 14),
-        GridItem(.fixed(120), spacing: 14)
+        GridItem(.fixed(105), spacing: 14),
+        GridItem(.fixed(105), spacing: 14),
+        GridItem(.fixed(105), spacing: 14)
     ]
     
     var body: some View {
         
-        ZStack{
+        // ZStack{
+        
+        //     RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+        //        .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
+        //        .brightness(0.65)
+        //        .frame(width: UIScreen.main.bounds.width - 32, height: 350)
+        
+        VStack(spacing: 10){
+            Text("Why?").font(.system(size: 24, weight: .heavy, design: .rounded)).padding()
             
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.init(red: 0.2, green: 0.2, blue: 0.2))
-                .brightness(0.65)
-                .frame(width: UIScreen.main.bounds.width - 32, height: 350)
-            
-            VStack(spacing: 10){
-                Text("Why?").font(.system(size: 24, weight: .heavy, design: .rounded))
-                
-                HStack{
-                    LazyVGrid(columns: items){
-                        ForEach(Reasons){ Reason in
-                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                                Text(Reason.name)
-                                    .font(.system(size: 16, weight: .heavy, design: .rounded))
-                                    .foregroundColor(.white)
-                                    .font(.headline)
-                                    .frame(width:100 ,height: 50)
-                                    .background(Color.blue)
-                                    .cornerRadius(20)
-
-                                
-                            }
-                            
-                           // .background(Color.blue)
+            HStack{
+                LazyVGrid(columns: items){
+                    ForEach(Reasons){ Reason in
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Text(Reason.name)
+                                .font(.system(size: 16, weight: .heavy, design: .rounded))
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .frame(width:100 ,height: 50)
+                                .background(Color.blue)
+                                .cornerRadius(20)
                             
                             
                         }
-                    }.padding(.leading)
-                        .padding(.trailing)
+                        
+                        
+                        
+                        
+                    }
                 }
-            }
+                
+            }.padding(.bottom)
         }
+        .frame(width: UIScreen.main.bounds.width - 32)
+        .background(Color.gray)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+        .padding()
+        
         
     }
 }
